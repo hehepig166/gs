446710db252ff790563761fb4a4a818ccb85dc92
hehepig166 <1260647889@qq.com>
2024-04-22 20:02:46 +0800

    makefile

--- train.py
+++ train.py
@@ -85,8 +85,11 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
         render_pkg = render(viewpoint_cam, gaussians, pipe, bg)
         image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
+
         thickness = render_pkg["tmpinfo"][1, :, :]
         Lthickness = torch.mean(thickness)
+        if iteration < 2000:
+            Lthickness = 0
 
         # Loss
         gt_image = viewpoint_cam.original_image.cuda()
